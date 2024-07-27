1d2484b79437e0b1383097bb18d8ef0b06afaf75
Eiad <eo.code.wd@gmail.com>
2024-07-27 18:51:13 +0000

    Sat, Jul 27, 2024, 8:51 PM +02:00

---
+++ einstein/einstein.py
@@ -0,0 +1,16 @@
+def main():
+    strM = input("M: ")
+    output= findE(int(strM))
+
+    print(f"E: {outPut}")
+
+def convert(input):
+    if input:
+        c =300000000
+        outPut= input*c
+        return
+
+
+
+
+main()

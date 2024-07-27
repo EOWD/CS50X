aed88026bf425ef17400cf3e1b31682a66b6d08b
Eiad <eo.code.wd@gmail.com>
2024-07-27 21:11:25 +0000

    Sat, Jul 27, 2024, 11:11 PM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,7 +1,7 @@
 def main():
     user_input=input("What is the answer of the great question of Life, the Universe, and Everything?")
-   # new_input=user_input.lower().strip().replace(" ","").replace("-","")
-    if "42" in user_input  or ("forty" in user_input and "two" in user_input):
+   new_input=user_input.lower().strip()
+    if "42" in new_input  or ("forty" in new_input and "two" in new_input):
          print("Yes")
     else:
         print( "No")

cd16ea7e2cd8f6a56290a30759926110e2dcaf7c
Eiad <eo.code.wd@gmail.com>
2024-07-27 20:46:20 +0000

    Sat, Jul 27, 2024, 10:46 PM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,2 +1,13 @@
 def main():
-    user_input=input()
+    user_input=input("what is the answer of the great question of life , the universe,and Everything?")
+    if user_input.lower()=="42" or user_input.lower()=="forty-two" or user_input.lower()=="forty two":
+         print("YES")
+    else:
+        print( "no")
+
+
+
+main()
+
+
+

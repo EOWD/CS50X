152295c66c55339fbaf882a88bf5d05d06129663
Eiad <eo.code.wd@gmail.com>
2024-07-27 21:50:04 +0000

    Sat, Jul 27, 2024, 11:50 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -11,3 +11,16 @@ def main():
 
 main()
 
+"""
+def main():
+    greeting = input("Greeting: ").strip().lower()
+# NOTE: can also access string in python similare to arrays in js
+    if greeting[:5] == "hello":
+        print("$0")
+    elif greeting[:1] == "h":
+        print("$20")
+    else:
+        print("$100")
+
+main()
+"""

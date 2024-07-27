023cb2cb00b942da6466366ed875419be9c01dc2
Eiad <eo.code.wd@gmail.com>
2024-07-27 18:32:58 +0000

    Sat, Jul 27, 2024, 8:32 PM +02:00

---
+++ faces/faces.py
@@ -0,0 +1,12 @@
+def main():
+    userInput= input("lets converts your smiles")
+    outPut= convert(userinput.strip())
+    print(outPut)
+
+def convert(input):
+    if input != "":
+        outPut=input.replace(":)","🙂")
+        outPut=input.replace(":)","🙂")
+        return outPut
+
+main()

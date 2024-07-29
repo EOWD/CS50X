a31570ff78c926ccfdc85c1f414b950016fe1ac8
Eiad <eo.code.wd@gmail.com>
2024-07-29 15:19:00 +0000

    Mon, Jul 29, 2024, 5:19 PM +02:00

---
+++ extentions/extentions.py
@@ -0,0 +1,28 @@
+def main():
+    fileName=input('Give filename: ')
+    fileExtention= checkFileExtention(fileName)
+    print(f"file type: {fileExtention}")
+
+def checkFileExtention(fileName):
+
+    types={
+        '.gif': 'image/gif',
+        '.jpg': 'image/jpeg',
+        '.jpeg': 'image/jpeg',
+        '.png': 'image/png',
+        '.pdf': 'application/pdf',
+        '.txt': 'text/plain',
+        '.zip': 'application/zip'
+    }
+    name = fileName.lower().strip()
+    for key in types:
+        if key in name:
+            return(types[key])
+    return "application/octet-stream"
+
+
+
+
+main()
+
+

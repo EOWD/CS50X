ac99e312fd35b0b7c9f6d5266e28847f903fa087
Eiad <eo.code.wd@gmail.com>
2024-08-14 16:36:43 +0000

    Wed, Aug 14, 2024, 6:36 PM +02:00

---
+++ credit/credit.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void validate(long n);
+int main(void)
+{
+    long input = get_long("Number: ");
+    validate(input);
+}
+
+void validate(long n)
+{
+long num = n / 10;
+printf("%li",num);
+}

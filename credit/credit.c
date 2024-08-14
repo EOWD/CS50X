8f5614c491f9d8f24eaedbfada108b5105a99c49
Eiad <eo.code.wd@gmail.com>
2024-08-14 19:10:50 +0000

    Wed, Aug 14, 2024, 9:10 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,28 +7,26 @@ int main(void)
 {
     long input = get_long("Number: ");
     bool val = validate(input);
-    printf("%s\n", val?"true":"false");
-    if(!val)
+    printf("%s\n", val ? "true" : "false");
+    if (!val)
     {
         printf("INVALID\n");
-
     }
     else
     {
-        printf("%s",card(input));
+        printf("%s", card(input));
     }
-
 }
 
 bool validate(long n)
 {
     long everySec = 0;
     long num = n / 10;
-    //printf("%li",num);
-    while(num>0)
+    // printf("%li",num);
+    while (num > 0)
     {
-        long number=num%10;
-        everySec=everySec * 10 + number;
+        long number = num % 10;
+        everySec = everySec * 10 + number;
 
         num = num / 100;
     }
@@ -36,69 +34,65 @@ bool validate(long n)
 
     long ma = 0;
     long newEverySec = everySec;
-    while(newEverySec>0)
+    while (newEverySec > 0)
     {
-        long currentNum=newEverySec % 10;
-        int m=currentNum*2;
-        if(m>9)
+        long currentNum = newEverySec % 10;
+        int m = currentNum * 2;
+        if (m > 9)
         {
-            while(m>0)
+            while (m > 0)
             {
-               int newNum=m%10;
-               ma+=newNum;
-               m=m/10;
+                int newNum = m % 10;
+                ma += newNum;
+                m = m / 10;
             }
-        }else{
-            ma+=m;
-
-
         }
-       newEverySec=newEverySec/10;
-
+        else
+        {
+            ma += m;
+        }
+        newEverySec = newEverySec / 10;
     }
-    printf("%li\n",ma);
-    printf("%li\n",n);
-    int result=ma;
-    long otherNums= n;
-    while(otherNums>0)
+    printf("%li\n", ma);
+    printf("%li\n", n);
+    int result = ma;
+    long otherNums = n;
+    while (otherNums > 0)
     {
         int currentNum = otherNums % 10;
-        result+=currentNum;
-        otherNums=otherNums/100;
-
+        result += currentNum;
+        otherNums = otherNums / 100;
     }
-    printf("%i",result);
+    printf("%i", result);
     const int res = result % 10;
     return res == 0;
-
-
 }
 
 string card(long n)
 {
-    long countN=n;
-    int count=0;
-    while(countN>0){
-        countN/=10;
+    long countN = n;
+    int count = 0;
+    while (countN > 0)
+    {
+        countN /= 10;
         count++;
     }
-    long cardNum=n;
-    while(cardNum>=100)
+    long cardNum = n;
+    while (cardNum >= 100)
     {
-        cardNum=cardNum/10;
+        cardNum = cardNum / 10;
     }
-    if((cardNum==34||cardNum==37)&&count==15)
+    if ((cardNum == 34 || cardNum == 37) && count == 15)
     {
         return "AMEX\n";
-
     }
-    else if ((cardNum>=51&&cardNum<=55)&&count==16)
+    else if ((cardNum >= 51 && cardNum <= 55) && count == 16)
     {
         return "MASTERCARD\n";
     }
-    else if((cardNum / 10 == 4) && (count==16||count==13))
+    else if ((cardNum / 10 == 4) && (count == 16 || count == 13))
     {
-       return "VISA\n";
+        return "VISA\n";
     }
     else
     {

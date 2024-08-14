a56aa1bee7db82ba4bbd569fc3d8f5fbb05a5ce6
Eiad <eo.code.wd@gmail.com>
2024-08-14 13:05:53 +0000

    Wed, Aug 14, 2024, 3:05 PM +02:00

--- me/hello.c
+++ me/hello.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
-#include<cs50.h>
+#include <cs50.h>
+#include <stdio.h>
 void sayHello(string input);
 int main(void)
 {
-    do {
-        string input=get_string("What's your name? ");
-    }while(input==NULL)
+    string input;
+    do
+    {
+        input = get_string("What's your name? ");
+    }
+    while (input == NULL);
 
     sayHello(input);
-
 }
-
 
 void sayHello(string i)
 {
-printf("hello, %s\n", i);
+    printf("hello, %s\n", i);
 }

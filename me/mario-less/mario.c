3f58829916a124cf7eefdaad0b4ca3d7d10cb70f
Eiad <eo.code.wd@gmail.com>
2024-08-14 13:56:56 +0000

    Wed, Aug 14, 2024, 3:56 PM +02:00

--- me/mario-less/mario.c
+++ me/mario-less/mario.c
@@ -1,5 +1,5 @@
-#include<cs50.h>
-#include<stdio.h>
+#include <cs50.h>
+#include <stdio.h>
 void buildBlocks(int n);
 int main(void)
 {
@@ -8,25 +8,24 @@ int main(void)
     {
         input = get_int("Height: ");
     }
-    while(input<1);
+    while (input < 1);
     buildBlocks(input);
 }
 void buildBlocks(int n)
 {
-    string space=" ";
-    string block="#";
+    string space = " ";
+    string block = "#";
 
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0; j<n-i-1; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
-           printf("%s", space);
+            printf("%s", space);
         }
-         for(int k=0; k<=i; k++)
+        for (int k = 0; k <= i; k++)
         {
-           printf("%s", block);
+            printf("%s", block);
         }
         printf("\n");
-
     }
 }

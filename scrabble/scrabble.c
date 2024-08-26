74d68ff003ab3d61377c5d3c50b841c08a2765f5
Eiad <eo.code.wd@gmail.com>
2024-08-26 11:20:23 +0000

    Mon, Aug 26, 2024, 1:20 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,5 +5,9 @@ int main(void)
 {
     int d[5];
     int array_len=sizeof(d)/sizeof(d[0]);
-    printf("%i \n"array_len);
+    printf("%i \n",array_len);
+    for (int i=0;i<array_len;i++)
+    {
+        d[i]=
+    }
 }

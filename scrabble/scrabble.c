91ca973e7c3b5cbb7f20c02f3688ff7bbf786245
Eiad <eo.code.wd@gmail.com>
2024-08-26 11:31:31 +0000

    Mon, Aug 26, 2024, 1:31 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
-
+#include <string.h>
 int main(void)
 {
     int d[5];
-    d[0]=1
+    d[0]=1;
     int array_len=sizeof(d)/sizeof(d[0]);
-    printf("%i \n",array_len);
+   // printf("%i \n",array_len);
     for (int i=1;i<array_len;i++)
     {
-        d[i]=d[i-1]*2
+        d[i]=d[i-1]*2;
+        printf("%i ",d[i]);
     }
-    printf("%a)
+printf("\n");
 }

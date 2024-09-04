31d11b319d09942d711aa97209373d6d93baf186
Eiad <eo.code.wd@gmail.com>
2024-09-04 14:40:25 +0000

    Wed, Sep 4, 2024, 5:40 PM +03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <strlib.h>
 int main(int argv, string argc[])
 {
     printf("%s\n", argc[1]);
-    int d[int(argc[1])];
+    int d[atoi(argc[1])];
     d[0]=1;
     int array_len=sizeof(d)/sizeof(d[0]);
    // printf("%i \n",array_len);
